fbf82963da0dd401d0badd0ba1c951febaaab853
igor84 <stojkovic.igor@gmail.com>
2016-05-02 17:39:19 +0200

    Added support for recognizing keywords, identifiers and integers

--- compiler/smmtest.cpp
+++ compiler/smmtest.cpp
@@ -1,11 +1,24 @@
 #include "smmlexer.h"
 
+static char filebuf[1024];
+
 int main() {
-	PSmmLexer lex = smmInitLexer(NULL);
-	while (*lex->curChar != 0) {
-		smmNextChar(lex);
-		smmSkipWhitespaceFromStdIn(lex);
+	FILE* f = fopen("test.smm", "rb");
+	if (!f) {
+		printf("Failed opening file\n");
+		return -1;
+	}
+	fread(filebuf, sizeof(char), 1023, f);
+	fclose(f);
+	PSmmLexer lex = smmInitLexer(filebuf);
+	PSmmToken token = smmGetNextToken(lex);
+	printf("\n");
+	while (token->tokenType != smmEof) {
+		printf("Got token %d with repr %.100s and intval=%lld\n", token->tokenType, token->repr, token->intVal);
+		token = smmGetNextToken(lex);
 	}
 
-	printf("\nScanned %lld\n", lex->scanCount);
+	printf("\nScanned %lld chars\n", lex->scanCount);
+
+	return 0;
 }
