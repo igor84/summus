02e210988912192f7f013c249f0e08d7025c645e
igor84 <stojkovic.igor@gmail.com>
2016-07-30 12:02:36 +0200

    Optimizes stack memory usage in main function

--- compiler/smmtest.c
+++ compiler/smmtest.c
@@ -69,27 +69,33 @@ void printNode(PSmmAstNode node, int align) {
 	}
 }
 
-int main(int argc, char **argv) {
-	const char* filename = "console";
+PSmmAstNode loadModule(bool isFile, const char* filename, PSmmAllocator a) {
 	char* buf = NULL;
 	char filebuf[64 * 1024] = { 0 };
-	if (argc > 1) {
-		filename = argv[1];
+	if (isFile) {
 		FILE* f = fopen(filename, "rb");
 		if (!f) {
 			printf("Can't find %s in the current folder!\n", filename);
-			return EXIT_FAILURE;
+			exit(EXIT_FAILURE);
 		}
 		fread(filebuf, 1, 64 * 1024, f);
 		fclose(f);
 		buf = filebuf;
 	}
-	PSmmAllocator allocator = smmCreatePermanentAllocator(filename, 64 * 1024);
-	PSmmLexer lex = smmCreateLexer(buf, filename, allocator);
-	
-	PSmmParser parser = smmCreateParser(lex, allocator);
+	PSmmLexer lex = smmCreateLexer(buf, filename, a);
 
-	PSmmAstNode program = smmParse(parser);
+	PSmmParser parser = smmCreateParser(lex, a);
+
+	return smmParse(parser);
+}
+
+int main(int argc, char **argv) {
+	const char* filename = "console";
+	if (argc > 1) {
+		filename = argv[1];
+	}
+	PSmmAllocator allocator = smmCreatePermanentAllocator(filename, 64 * 1024);
+	PSmmAstNode program = loadModule(argc > 1, filename, allocator);
 
 	puts("\n");
 	printNode(program, 0);
